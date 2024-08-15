8cdf851613175b40b9e4ef5c8bec8e0366c4bb82
mattoofahad <mattoofahad@gmail.com>
2024-08-15 23:30:58 +0500

    updating

--- src/utils/openai_utils.py
+++ src/utils/openai_utils.py
@@ -16,20 +16,19 @@ class OpenAIFunctions:
         logger.info("Checking OpenAI Key")
         try:
             client = OpenAI(api_key=st.session_state.openai_api_key)
-            try:
-                client.models.list()
-                logger.error("OpenAI key Working")
-                return True
-            except openai.AuthenticationError as auth_error:
-                with st.chat_message("assistant"):
-                    st.error(str(auth_error))
-                logger.error("AuthenticationError: %s", auth_error)
-                return False
-            except openai.OpenAIError as openai_error:
-                with st.chat_message("assistant"):
-                    st.error(str(openai_error))
-                logger.error("OpenAIError: %s", openai_error)
-                return False
+            client.models.list()
+            logger.error("OpenAI key Working")
+            return True
+        except openai.AuthenticationError as auth_error:
+            with st.chat_message("assistant"):
+                st.error(str(auth_error))
+            logger.error("AuthenticationError: %s", auth_error)
+            return False
+        except openai.OpenAIError as openai_error:
+            with st.chat_message("assistant"):
+                st.error(str(openai_error))
+            logger.error("OpenAIError: %s", openai_error)
+            return False
         except Exception as general_error:
             with st.chat_message("assistant"):
                 st.error(str(general_error))
