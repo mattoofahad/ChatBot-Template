6ce2da5b8ee8b7fe7d1687279a4a8ea5d2cbd516
mattoofahad <mattoofahad@gmail.com>
2024-09-22 05:12:45 +0500

    replacing openai compeltion with litellm

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -10,8 +10,9 @@ class ConstantVariables:
         "gpt-4o",
         "gpt-4o-mini",
         "gpt-4-turbo",
-        "gpt-4",
         "gpt-3.5-turbo",
+        "o1-preview",
+        "o1-mini"
     )
     default_model = "gpt-4o-mini"
 
