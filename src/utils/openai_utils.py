55bd385623c603def94ef6166565bdf2bdf25247
mattoofahad <mattoofahad@gmail.com>
2024-10-11 00:13:03 +0500

    resolving linting error

--- src/utils/openai_utils.py
+++ src/utils/openai_utils.py
@@ -59,25 +59,24 @@ class OpenAIFunctions:
         if st.session_state.provider_select == "lm-studio":
             logger.info("Local Provider is Sekected")
             return True
-        else:
-            logger.info("Checking OpenAI Key")
-            try:
-                client = OpenAI(api_key=st.session_state.openai_api_key)
-                client.models.list()
-                logger.debug("OpenAI key Working")
-                return True
-            except openai.AuthenticationError as auth_error:
-                with st.chat_message("assistant"):
-                    st.error(str(auth_error))
-                logger.error("AuthenticationError: %s", auth_error)
-                return False
-            except openai.OpenAIError as openai_error:
-                with st.chat_message("assistant"):
-                    st.error(str(openai_error))
-                logger.error("OpenAIError: %s", openai_error)
-                return False
-            except Exception as general_error:
-                with st.chat_message("assistant"):
-                    st.error(str(general_error))
-                logger.error("Unexpected error: %s", general_error)
-                return False
+        logger.info("Checking OpenAI Key")
+        try:
+            client = OpenAI(api_key=st.session_state.openai_api_key)
+            client.models.list()
+            logger.debug("OpenAI key Working")
+            return True
+        except openai.AuthenticationError as auth_error:
+            with st.chat_message("assistant"):
+                st.error(str(auth_error))
+            logger.error("AuthenticationError: %s", auth_error)
+            return False
+        except openai.OpenAIError as openai_error:
+            with st.chat_message("assistant"):
+                st.error(str(openai_error))
+            logger.error("OpenAIError: %s", openai_error)
+            return False
+        except Exception as general_error:
+            with st.chat_message("assistant"):
+                st.error(str(general_error))
+            logger.error("Unexpected error: %s", general_error)
+            return False
